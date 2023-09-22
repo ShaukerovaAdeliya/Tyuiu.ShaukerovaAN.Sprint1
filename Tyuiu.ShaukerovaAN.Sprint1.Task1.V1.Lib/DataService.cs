b677ef6d9b064aeb5eda886e3316838cf903d9f8
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-09-22 10:06:37 +0500

    Task1

---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShaukerovaAN.Sprint1.Task1.V1.Lib
+{
+    public class DataService : ISprint1Task1V1
+    {
+        public double Calculate(double x, double y, double a)
+        {
+            return x / 3 / y + 6 * a;
+        }
+    }
+}
---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShaukerovaAN.Sprint1.Task1.V1.Lib;
+
+namespace Tyuiu.ShaukerovaAN.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 9.0;
+            double y = 3.0;
+            double a = 4.0;
+            var res = ds.Calculate(x, y, a);
+            Assert.AreEqual(25, res);
+
+        }
+    }
+}
