5cb89c1e0691651f62e29668c275f51f4e3cbd2d
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-09-29 00:46:12 +0500

    Task6

---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task6.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShaukerovaAN.Sprint1.Task6.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShaukerovaAN.Sprint1.Task6.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39a83713-1b78-4c02-90e3-54fb8ab1a9e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaukerovaAN.Sprint1.Task6.V18.Lib;
+
+// ЗАДАНИЕ
+// Написать программу: пользователь вводит текст.
+// Проверить, что строка является символьным представлением натурального числа.
+
+namespace Tyuiu.ShaukerovaAN.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                              *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                          *");
+            Console.WriteLine("* Проверить, что строка является символьным                               *");
+            Console.WriteLine("* представлением натурального числа.                                      *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите строку");
+
+            string x = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            bool res = ds.CheckNumber(x);
+            if (res == true)
+            {
+                Console.WriteLine("Натуральное число");
+            }
+            if (res == false)
+            {
+                Console.WriteLine("Не натуральное число");
+            }
+              
+            Console.ReadKey();
+        }
+    }
+}
