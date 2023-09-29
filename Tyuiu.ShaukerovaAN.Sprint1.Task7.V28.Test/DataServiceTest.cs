a54e708564feef4dc0e16434b6d63ca193ca881b
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-09-29 22:55:06 +0500

    Task7

---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShaukerovaAN.Sprint1.Task7.V28.Lib;
+namespace Tyuiu.ShaukerovaAN.Sprint1.Task7.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 4;
+            double wait = 16.948;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaukerovaAN.Sprint1.Task7.V28/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaukerovaAN.Sprint1.Task7.V28.Lib;
+
+// ЗАДАНИЕ
+// Написать программу, которая вычисляет математическое выражение по исходным значениям данных,
+// вводимых пользователем. Ответ округлите до 3 знаков после запятой.
+// z = cos^2(sin 1/x) + y^x
+
+namespace Tyuiu.ShaukerovaAN.Sprint1.Task7.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #28                                                              *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("*                                                                         *");
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* z = cos^2(sin(1/x) + y^x                                                *");
+            Console.WriteLine("*                                                                         *");
+
+            DataService ds = new DataService();
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
